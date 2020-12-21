ac74db1fa2537fc471cb460c79fc6d5ae9fd3fc7
kaleongtongproject <kaleongtong@hotmail.com>
2020-12-21 00:54:21 -0800

    init project structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MainPage from './views/MainPage';
+import 'font-awesome/css/font-awesome.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
